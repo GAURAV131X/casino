eac5aa6b62a94cf09f8ffd03beab16a71ed4db50
GAURAV131X <sonigaurav2207@gmail.com>
2021-03-14 15:33:48 +0530

    Login Added

--- src/App.js
+++ src/App.js
@@ -2,73 +2,114 @@ import "./App.css";
 import "../node_modules/bootstrap/dist/css/bootstrap.css";
 import React from "react";
 import Popup from "./Popup";
+import Login from "./Login";
 
 class App extends React.Component {
   constructor() {
     super();
-   
+
     this.state = {
       showPopup: false,
-      slot1:"",
-      slot2:"",
-      slot3:"",
-      balance: 0,
-      currentTime:""
+      showLoginPopup: false,
+      balance: 99,
+      name: "Guest",
+      slots: [
+        {
+          slot1: "",
+          slot2: "",
+          slot3: "",
+          currentTime: "",
+        },
+      ],
     };
   }
-  cb = (s1, s2, s3) => {
-    this.setState({ slot1: s1, slot2: s2, slot3: s3 });
-    var bal = 0;
 
-    if (s1 === 3 && s2 === 3 && s3 === 3) {
-      bal = bal + 5;
-    } else if (s1 === 7 && s2 === 7 && s3 === 7) {
-      bal = bal + 10;
+  // sorting
+  compareBy(key) {
+    return function (a, b) {
+      if (a[key] < b[key]) return -1;
+      if (a[key] > b[key]) return 1;
+      return 0;
+    };
+  }
+  //sorting
+  sortBy(key) {
+    let arrayCopy = [...this.state.slots];
+    arrayCopy.sort(this.compareBy(key));
+    this.setState({ slots: arrayCopy });
+    console.log("sort fun");
+  }
+  //callback for updating state
+  cb = (s1, s2, s3) => {
+    var today = new Date(),
+      time =
+        today.getHours() + ":" + today.getMinutes() + ":" + today.getSeconds();
+    this.setState({
+      slots: [...this.state.slots, { s1, s2, s3, time }],
+    });
+
+    var bal = 0;
+    // console.log(this.state.slots);
+    if (s1 === s2 && s2 === s3 && s3 === 7) {
+      bal = 9;
+    } else if (s1 === s2 && s2 === s3) {
+      bal = 4;
+    } else if (s1 === s2 || s2 === s3) {
+      bal = -0.5;
+    } else {
+      bal = -1;
     }
 
-    this.setState({ balance: bal });
-    var today = new Date(),
-
-    time = today.getHours() + ':' + today.getMinutes() + ':' + today.getSeconds();
-    this.setState({currentTime:time})
+    this.setState({ balance: this.state.balance + bal });
   };
+
+  cbLogin = (n) => {
+    this.setState({
+      name: n,
+    });
+  };
+
   togglePopup() {
     this.setState({
       showPopup: !this.state.showPopup,
     });
   }
+
+  toggleLoginPopup() {
+    this.setState({
+      showLoginPopup: !this.state.showLoginPopup,
+    });
+  }
+
   render() {
     return (
       <>
         <nav className="navbar navbar-expand-lg navbar-dark bg-primary">
           <div className="container">
             <span className="navbar-brand">Casino</span>
-
-            {/* <span className="btn btn-outline-light" >Balance</span> */}
-            <div class="d-flex">
-              <span class="navbar-brand">Balance</span>
+            <div className="d-flex">
+              <span className="navbar-brand">Balance</span>
               <input
-                class="form-control me-2"
-                disabled="true"
+                className="form-control me-2"
+                readOnly
                 value={this.state.balance}
               />
-
-              <a
-                class="nav-link dropdown-toggle"
-                href="#"
-                id="navbarDropdownMenuLink"
-                role="button"
-                data-toggle="dropdown"
-                aria-haspopup="true"
-                aria-expanded="false"
-              >
-                <img
-                  src="https://s3.eu-central-1.amazonaws.com/bootstrapbaymisc/blog/24_days_bootstrap/fox.jpg"
+              <>
+                <button
                   width="40"
                   height="40"
-                  class="rounded-circle"
-                />
-              </a>
+                  className="rounded-circle"
+                  onClick={this.toggleLoginPopup.bind(this)}
+                >
+                  <img
+                    src="https://s3.eu-central-1.amazonaws.com/bootstrapbaymisc/blog/24_days_bootstrap/fox.jpg"
+                    width="40"
+                    height="40"
+                    className="rounded-circle"
+                    alt="img"
+                  />
+                </button>
+              </>
             </div>
           </div>
         </nav>
@@ -76,12 +117,10 @@ class App extends React.Component {
           <div className="py-4">
             <nav className="navbar navbar-expand-lg navbar-light bg-light">
               <div className="container">
-                <span className="navbar-brand">Casino</span>
-
-                {/* <span className="btn btn-outline-light" >Balance</span> */}
-                <div class="d-flex">
+                <span className="navbar-brand">{this.state.name}</span>
+                <div className="d-flex">
                   <button
-                    class="btn btn-outline-primary"
+                    className="btn btn-outline-primary"
                     onClick={this.togglePopup.bind(this)}
                   >
                     Start
@@ -90,47 +129,27 @@ class App extends React.Component {
               </div>
             </nav>
 
-            <table class="table border shadow">
-              <thead class="thead-dark">
+            <table className="table border shadow">
+              <thead className="thead-dark">
                 <tr>
                   <th scope="col">#</th>
                   <th scope="col">Slot 1</th>
                   <th scope="col">Slot 2</th>
                   <th scope="col">Slot 3</th>
                   <th scope="col">Time</th>
+                  {/* <button onClick={() => this.sortBy('slot1')}>mhvhnv</button> */}
                 </tr>
               </thead>
               <tbody>
-              <th scope="row">#</th>
-              <td>{this.state.slot1}</td>
-              <td>{this.state.slot2}</td>
-              <td>{this.state.slot3}</td>
-              <td>{ this.state.currentTime}</td>
-                {/* {users.map((user, index) => (
-            <tr>
-              <th scope="row">{index + 1}</th>
-              <td>{user.name}</td>
-              <td>{user.course}</td>
-              <td>{user.email}</td>
-              <td>
-                <Link class="btn btn-primary mr-2" to={`/users/${user._id}`}>
-                  View
-                </Link>
-                <Link
-                  class="btn btn-outline-primary mr-2"
-                  to={`/users/edit/${user._id}`}
-                >
-                  Edit
-                </Link>
-                <Link
-                  class="btn btn-danger"
-                  onClick={() => deleteUser(user._id)}
-                >
-                  Delete
-                </Link>
-              </td>
-            </tr>
-          ))} */}
+                {this.state.slots.map((item, index) => (
+                  <tr key={index}>
+                    <th scope="row">{index}</th>
+                    <td>{item.s1}</td>
+                    <td>{item.s2}</td>
+                    <td>{item.s3}</td>
+                    <td>{item.time}</td>
+                  </tr>
+                ))}
               </tbody>
             </table>
           </div>
@@ -139,7 +158,12 @@ class App extends React.Component {
         {this.state.showPopup ? (
           <Popup pcb={this.cb} closePopup={this.togglePopup.bind(this)} />
         ) : null}
-
+        {this.state.showLoginPopup ? (
+          <Login
+            login={this.cbLogin}
+            closeLoginPopup={this.toggleLoginPopup.bind(this)}
+          />
+        ) : null}
         <div className="footer">copyright</div>
       </>
     );
